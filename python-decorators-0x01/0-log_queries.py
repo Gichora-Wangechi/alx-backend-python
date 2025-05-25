84f70753480fa0c7a922f1928f305263aa42ec1d
Gichora-Wangechi <annalexisgichora@gmail.com>
2025-05-25 21:33:04 +0300

    Update 0-log_queries.py

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,1 +1,23 @@
+import sqlite3
+import functools
 
+def log_queries(func):
+    """Decorator to log SQL queries before executing them"""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query = kwargs.get('query') or (args[0] if args else "")
+        print(f"Executing query: {query}")
+        return func(*args, **kwargs)
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+# Fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
