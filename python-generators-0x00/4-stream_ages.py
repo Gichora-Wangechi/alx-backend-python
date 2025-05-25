00132a1f8474b1829fbc108cafbff4975db8e93e
Gichora-Wangechi <annalexisgichora@gmail.com>
2025-05-25 23:46:45 +0300

    Update 4-stream_ages.py

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+import mysql.connector
 
+def stream_ages():
+    """Generator that yields ages of all users in the database."""
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="your_mysql_user",
+        password="your_mysql_password",
+        database="ALX_prodev"
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT age FROM user_data")
+
+    for (age,) in cursor:
+        yield float(age)
+
+    cursor.close()
+    connection.close()
