4d3401b7fa7b46182278b08c4d4b95b17d24cfbe
Gichora-Wangechi <annalexisgichora@gmail.com>
2025-05-25 23:46:00 +0300

    Update 2-lazy_paginate.py

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python3
+import mysql.connector
 
+def lazy_paginate(page_size=10):
+    """Yields pages of user data from the database."""
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="your_mysql_user",
+        password="your_mysql_password",
+        database="ALX_prodev"
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM user_data")
+
+    while True:
+        rows = cursor.fetchmany(size=page_size)
+        if not rows:
+            break
+        yield rows
+
+    cursor.close()
+    connection.close()
