b12ff076efd79dfec3b39ecc49e19944910dd59d
Gichora-Wangechi <annalexisgichora@gmail.com>
2025-06-01 20:15:14 +0300

    Update serializers.py

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,1 +1,22 @@
+from rest_framework import serializers
+from .models import User, Conversation, Message
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['id', 'sender', 'conversation', 'content', 'timestamp']
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['id', 'participants', 'messages', 'created_at']
