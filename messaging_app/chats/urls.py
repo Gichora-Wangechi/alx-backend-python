ea0470997308bb329ca02373caa8cdc40dd9b24e
Gichora-Wangechi <annalexisgichora@gmail.com>
2025-06-01 20:20:46 +0300

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -9,3 +9,11 @@ router.register(r'messages', MessageViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),
+]
